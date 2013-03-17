5ab6cb2fc560c8d0a35915afa9f3b925735b0f65
Jéferson Machado <jefersonm@gmail.com>
2013-03-16 23:39:33 -0300

    Add SpringMVC project

---
+++ java/spring-mvc-example/src/main/java/com/github/jefersonm/spring/mvc/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.github.jefersonm.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String home() {
+		System.out.println("HomeController: Passing through...");
+		return "WEB-INF/views/home.jsp";
+	}
+}
