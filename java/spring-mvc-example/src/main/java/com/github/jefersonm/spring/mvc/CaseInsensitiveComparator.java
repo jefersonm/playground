c25d97e3c55f4d3708de81e75c73a645982811a6
Jéferson Machado <jefersonm@gmail.com>
2013-03-17 00:06:17 -0300

    Improve code with more examples

---
+++ java/spring-mvc-example/src/main/java/com/github/jefersonm/spring/mvc/CaseInsensitiveComparator.java
@@ -0,0 +1,12 @@
+package com.github.jefersonm.spring.mvc;
+
+import java.util.Comparator;
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class CaseInsensitiveComparator implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        assert s1 != null && s2 != null;
+        return String.CASE_INSENSITIVE_ORDER.compare(s1, s2);
+    }
+}
