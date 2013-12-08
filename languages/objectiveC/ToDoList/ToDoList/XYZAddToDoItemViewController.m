fec1429161bf2461938013a3b42060318f633778
Jeferson Machado <jefersonm@gmail.com>
2013-12-08 21:56:16 -0200

    Adding more files

---
+++ languages/objectiveC/ToDoList/ToDoList/XYZAddToDoItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  XYZAddToDoItemViewController.m
+//  ToDoList
+//
+//  Created by Jéferson Machado on 07/12/13.
+//
+//
+
+#import "XYZAddToDoItemViewController.h"
+
+@interface XYZAddToDoItemViewController ()
+
+@end
+
+@implementation XYZAddToDoItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ languages/objectiveC/ToDoList/XYZToDoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZToDoListViewController.h
+//  ToDoList
+//
+//  Created by Jéferson Machado on 07/12/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZToDoListViewController : UITableViewController
+
+@end
