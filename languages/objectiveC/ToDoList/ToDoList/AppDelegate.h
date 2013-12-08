2cf2dc7dea90b151743b95322fcdd872ce30f89f
Jeferson Machado <jefersonm@gmail.com>
2013-12-07 22:26:10 -0200

    ToDoList project - iOS

---
+++ languages/objectiveC/ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Jéferson Machado on 07/12/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ languages/objectiveC/ToDoList/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Jéferson Machado on 07/12/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
