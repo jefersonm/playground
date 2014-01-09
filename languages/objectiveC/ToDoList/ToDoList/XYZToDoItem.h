06dd8c846eff6e5b8789db2933ef23fde3a847fd
Jeferson Machado <jefersonm@gmail.com>
2014-01-09 07:53:14 -0200

    Adding new files

---
+++ languages/objectiveC/ToDoList/ToDoList/XYZToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  XYZToDoItem.h
+//  ToDoList
+//
+//  Created by Jéferson Machado on 09/12/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZToDoItem : NSObject
+
+@end
---
+++ languages/objectiveC/ToDoList/ToDoList/XYZToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  XYZToDoItem.m
+//  ToDoList
+//
+//  Created by Jéferson Machado on 09/12/13.
+//
+//
+
+#import "XYZToDoItem.h"
+
+@implementation XYZToDoItem
+
+@end
