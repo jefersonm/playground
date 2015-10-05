498994e2ee708006a3852b15620fe31cbc34910a
Jeferson Machado <jefersonm@gmail.com>
2015-10-05 15:17:11 -0300

    melhorando strutura de testes

--- DOJOs/dojo5/dojo_app/app/converter.js
+++ DOJOs/dojo5/dojo_app/app/converter.js
@@ -1,17 +1,3 @@
-exports.rgbToHex = function(red, green, blue) {
-
-  var redHex   = red.toString(16);
-  var greenHex = green.toString(16);
-  var blueHex  = blue.toString(16);
-
-  return pad(redHex) + pad(greenHex) + pad(blueHex);
-
-};
-
-function pad(hex) {
-  return (hex.length === 1 ? "0" + hex : hex);
-}
-
 exports.hexToRgb = function(hex) {
 
   var red   = parseInt(hex.substring(0, 2), 16);
--- DOJOs/dojo5/dojo_app/test/converter.js
+++ DOJOs/dojo5/dojo_app/test/converter.js
@@ -2,18 +2,7 @@ var expect    = require("chai").expect;
 var converter = require("../app/converter");
 
 describe("Color Code Converter", function() {
-  describe("RGB to Hex conversion", function() {
-    it("converts the basic colors", function() {
-      var redHex   = converter.rgbToHex(255, 0, 0);
-      var greenHex = converter.rgbToHex(0, 255, 0);
-      var blueHex  = converter.rgbToHex(0, 0, 255);
-
-      expect(redHex).to.equal("ff0000");
-      expect(greenHex).to.equal("00ff00");
-      expect(blueHex).to.equal("0000ff");
-    });
-  });
-
+  
   describe("Hex to RGB conversion", function() {
     it("converts the basic colors", function() {
       var red   = converter.hexToRgb("ff0000");
